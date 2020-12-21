2eade9d209418842327d47ce32b133a29a034ed8
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2020-12-21 23:24:03 +0900

    https://www.acmicpc.net/problem/14226

--- DFS|BFS/이모티콘.py
+++ DFS|BFS/이모티콘.py
@@ -4,23 +4,24 @@ from collections import deque
 input = sys.stdin.readline
 
 s = int(input())
-visited = [[math.inf] * 1001 for _ in range(1001)]
+visited = {(1, 0): 0}
 
 def bfs(d, c): # display, clip board
   queue = deque()
-  visited[d][c] = 0
   queue.append((d, c))
   while queue:
     d, c = queue.popleft()
-    if visited[d][d] == math.inf:
-      visited[d][d] = visited[d][c] + 1
+    if d == s:
+      print(visited[d, c])
+      break
+    if (d, d) not in visited:
+      visited[(d, d)] = visited[(d, c)] + 1
       queue.append((d, d))
-    if d + c <= s and visited[d + c][c] == math.inf:
-      visited[d + c][c] = visited[d][c] + 1
+    if d + c <= s and (d + c, c) not in visited:
+      visited[(d + c, c)] = visited[(d, c)] + 1
       queue.append((d + c, c))
-    if d - 1 >= 0 and visited[d - 1][c] == math.inf:
-      visited[d - 1][c] = visited[d][c] + 1
+    if d - 1 >= 0 and (d - 1, c) not in visited:
+      visited[(d - 1, c)] = visited[(d, c)] + 1
       queue.append((d - 1, c))
 
 bfs(1, 0)
-print(min(visited[s]))
