8f7790b6ecffb6fadf72b6fd47250af843348bbe
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-01-04 21:26:59 +0900

    '이것이 취업을 위한 코딩 테스트다' 内 기출문제 4

---
+++ Greedy/만들_수_없는_금액.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+coins = list(map(int, input().split()))
+target = 1
+coins.sort()
+
+for coin in coins:
+  if target < coin: break
+  target += coin
+
+print(target)
