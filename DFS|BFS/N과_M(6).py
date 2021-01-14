ccff7fc199153d8a9a6758c827575c150d139c11
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-01-15 01:19:02 +0900

    https://www.acmicpc.net/problem/15655

---
+++ DFS|BFS/N과_M(6).py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+visited = [False] * n
+data = []
+
+def dfs(length):
+  if length == m:
+    print(*data)
+    return
+  for i in range(n):
+    if visited[i]:
+      continue
+    visited[i] = True
+    data.append(nums[i])
+    dfs(length + 1)
+    data.pop()
+    for j in range(i+1, n):
+      visited[j] = False
+dfs(0)
