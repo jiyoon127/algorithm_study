f26cac31b0863f098dc02ca9bb2a98722f848b0c
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-01-16 18:10:27 +0900

    https://www.acmicpc.net/problem/14891

--- Implementation/톱니바퀴.py
+++ Implementation/톱니바퀴.py
@@ -1,30 +1,34 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
-def turn(start, direction, next_step):
-  nei = start + next_step
-  if nei > 4 or nei < 1: return
-  if next_step == 1:
-    if(gears[start - 1][2] != gears[nei - 1][6]):
-      turn(nei, -direction, next_step)
-      gears[nei - 1].rotate(direction)
+def turn(target, direct):
+  if direct == -1:
+    wheels[target-1].append(wheels[target-1].popleft())
   else:
-    if(gears[start - 1][6] != gears[nei - 1][2]):
-      turn(nei, -direction, next_step)
-      gears[nei - 1].rotate(direction)
+    wheels[target-1].appendleft(wheels[target-1].pop())
 
-gears = []
-score = 0
+def turn_nei(target, direct, nei):
+  if nei > 4 or nei < 1: return
+  if target < nei and wheels[target - 1][2] != wheels[nei - 1][6]:
+    turn_nei(nei, -direct, nei + 1)
+    turn(nei, direct)
+  if target > nei and wheels[target - 1][6] != wheels[nei - 1][2]:
+    turn_nei(nei, -direct, nei - 1)
+    turn(nei, direct)
+
+wheels = []
 for _ in range(4):
-  gears.append(deque(map(int, list(input().strip()))))
-t = int(input())
-for _ in range(t):
-  start, direction = map(int, input().split())
-  turn(start, -direction, 1)
-  turn(start, -direction, -1)
-  gears[start - 1].rotate(direction)
+  wheels.append(deque(map(int,input().rstrip())))
 
+k = int(input())
+for i in range(k):
+  target, direct = map(int, input().split())
+  turn_nei(target, -direct, target + 1)
+  turn_nei(target, -direct, target - 1) 
+  turn(target, direct)
+
+ans = 0
 for i in range(4):
-  score += (2**i) * gears[i][0]
-print(score)
+  ans += wheels[i][0] * (2**i)
+print(ans)
