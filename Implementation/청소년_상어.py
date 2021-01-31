d20ed573feb51381a56d80afb507135768f04dc9
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-01-31 16:09:09 +0900

    https://www.acmicpc.net/problem/19236

--- Implementation/청소년_상어.py
+++ Implementation/청소년_상어.py
@@ -1,5 +1,7 @@
 import sys
 from copy import deepcopy
+
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
 fishes = []
@@ -11,8 +13,8 @@ for i in range(4):
     row = list(map(int, input().split()))
     graph.append([])
     for j in range(0, 8, 2):
-        graph[i].append([row[j], row[j + 1]])
-        fishes.append((row[j], row[j + 1], i, j // 2))
+        graph[i].append([row[j], row[j + 1] - 1])
+        fishes.append((row[j], row[j + 1] - 1, i, j // 2))
 fishes.sort()
 
 fishes[graph[0][0][0] - 1] = ()
@@ -22,13 +24,13 @@ graph[0][0][0] = -1
 
 def turn(x, y, direct):
     for _ in range(8):
-        nx = x + dx[(direct - 1) % 8]
-        ny = y + dy[(direct - 1) % 8]
+        nx = x + dx[direct]
+        ny = y + dy[direct]
         if 0 <= nx < 4 and 0 <= ny < 4 and graph[nx][ny][0] != -1:
-            return nx, ny, direct if direct != 0 else 8
+            return nx, ny, direct
         else:
             direct = (direct + 1) % 8
-    return x, y, False
+    return x, y, direct
 
 
 def move_fish():
@@ -37,7 +39,7 @@ def move_fish():
             cur, direct, x, y = fishes[i]
             nx, ny, n_direct = turn(x, y, direct)
 
-            if not n_direct: continue
+            if nx == x and ny == y: continue
             if graph[nx][ny][0]:
                 og, og_direct = graph[nx][ny]
                 graph[nx][ny] = [cur, n_direct]
@@ -55,8 +57,8 @@ def simulate(x, y, direct, ate):
 
     graph[x][y] = [0, 0]
     while True:
-        nx = x + dx[direct - 1]
-        ny = y + dy[direct - 1]
+        nx = x + dx[direct]
+        ny = y + dy[direct]
         if not 0 <= nx < 4 or not 0 <= ny < 4:
             ans = max(ans, ate)
             return
@@ -64,13 +66,13 @@ def simulate(x, y, direct, ate):
             x, y = nx, ny
             continue
 
-        new_graph, new_fishes = deepcopy(graph), deepcopy(fishes)
+        og_graph, og_fishes = deepcopy(graph), deepcopy(fishes)
         prey, p_direct = graph[nx][ny]
         fishes[prey - 1] = ()
         graph[nx][ny][0] = -1
         simulate(nx, ny, p_direct, ate + prey)
-        graph = new_graph
-        fishes = new_fishes
+        graph = og_graph
+        fishes = og_fishes
         x, y = nx, ny
 
 
