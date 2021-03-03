2c098614189f886ebc40640fe52895a20c5af149
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-03-03 22:34:00 +0900

    https://www.acmicpc.net/problem/2003

---
+++ Two_Pointers/수들의_합_2.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+cnt = 0
+interval_sum = 0
+end = 0
+
+for start in range(n):
+    while interval_sum < m and end < n:
+        interval_sum += a[end]
+        end += 1
+    if interval_sum == m: cnt += 1
+    interval_sum -= a[start]
+
+print(cnt)
