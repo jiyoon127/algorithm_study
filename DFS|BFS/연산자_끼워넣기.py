af219b1b20207cf47f3565741a22edc99a464f20
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-08 22:13:06 +0900

    '이것이 취업을 위한 코딩 테스트다' 内 기출문제 19

--- DFS|BFS/연산자_끼워넣기.py
+++ DFS|BFS/연산자_끼워넣기.py
@@ -1,38 +1,40 @@
 # 백준 연산자 끼워넣기
 # https://www.acmicpc.net/problem/14888
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-data = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
+def solution(i, result):
+    global _max, _min, add, sub, mul, div
+    if i == n:
+        _max = max(_max, result)
+        _min = min(_min, result)
+        return
 
-min_val = int(1e9)
-max_val = -int(1e9)
-
-def dfs(i, now):
-  global min_val, max_val, add, sub, mul, div
-  if i == n:
-    min_val = min(min_val, now)
-    max_val = max(max_val, now)
-  else:
     if add > 0:
-      add -= 1
-      dfs(i+1, now + data[i])
-      add += 1
+        add -= 1
+        solution(i + 1, result + nums[i])
+        add += 1
     if sub > 0:
-      sub -= 1
-      dfs(i+1, now - data[i])
-      sub += 1
+        sub -= 1
+        solution(i + 1, result - nums[i])
+        sub += 1
     if mul > 0:
-      mul -= 1
-      dfs(i+1, now * data[i])
-      mul += 1
+        mul -= 1
+        solution(i + 1, result * nums[i])
+        mul += 1
     if div > 0:
-      div -= 1
-      dfs(i+1, int(now / data[i]))
-      div += 1
+        div -= 1
+        solution(i + 1, int(result / nums[i]))
+        div += 1
 
-dfs(1, data[0])
-print(max_val, min_val, end = '\n')
+n = int(input())
+nums = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
+data = []
+INF = int(1e9)
+_max = -INF
+_min = INF
+
+solution(1, nums[0])
+
+print(_max, _min, sep = "\n")
