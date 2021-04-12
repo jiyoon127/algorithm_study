8453dd41d5d1397dea1194cae684da04159ba260
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-12 17:02:06 +0900

    https://www.acmicpc.net/problem/13458

--- Implementation/시험_감독.py
+++ Implementation/시험_감독.py
@@ -2,11 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-classroom = list(map(int, input().split()))
-b, c = map(int, input().split())
+classes = list(map(int, input().split()))
+_main, _sub = map(int, input().split())
+teachers = 0
 
-for i in range(n):
-    if classroom[i] - b > 0:
-        n += (classroom[i] - b) // c
-        if (classroom[i] - b) % c: n += 1
-print(n)
+for _class in classes:
+    rest = _class - _main
+    teachers += 1 # _main
+    if rest <= 0: continue
+    if rest % _sub: teachers += 1
+
+    teachers += rest // _sub
+
+print(teachers)
