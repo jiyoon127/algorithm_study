49dfbbedbbb81fc4e7bf7c990fc3bbfe43aaf15e
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-14 14:29:18 +0900

    https://www.acmicpc.net/problem/20061

--- Implementation/모노미노도미노_2.py
+++ Implementation/모노미노도미노_2.py
@@ -3,45 +3,47 @@ input = sys.stdin.readline
 
 def move(t, graph, column):
     global point
-    final_position = []
+
+    if not graph: graph.append([0, 0, 0, 0])
+    final_positions = []
     cur = len(graph) - 1
-    while cur >= 0:  # 쌓기
+    while cur >= 0:
         row = graph[cur]
         if t == 1:
-            if not row[column]: final_position = [(cur, column)]
+            if not row[column]: final_positions = [(cur, column)]
             else: break
         elif t == 2:
-            if (row[column], row[column + 1]) == (0, 0): final_position = [(cur, column), (cur, column + 1)]
+            if (row[column], row[column + 1]) == (0, 0):
+                final_positions = [(cur, column), (cur, column + 1)]
             else: break
         elif t == 3:
             if cur >= 1 and (row[column], graph[cur - 1][column]) == (0, 0):
-                final_position = [(cur, column), (cur - 1, column)]
+                final_positions = [(cur,  column), (cur - 1, column)]
             else: break
+
         cur -= 1
 
-    if not final_position:
+    if not final_positions:
         graph.append([0, 0, 0, 0])
-        if t == 1: final_position = [(-1, column)]
-        elif t == 2: final_position = [(-1, column), (-1, column + 1)]
+        if t == 1: final_positions = [(-1, column)]
+        elif t == 2:
+            final_positions = [(-1, column), (-1, column + 1)]
         else:
             if graph[-2][column]: graph.append([0, 0, 0, 0])
-            final_position = [(len(graph) - 1, column), (len(graph) - 2, column)]
+            final_positions = [(-2, column), (-1, column)]
 
-    for x, y in final_position:
+    for x, y in final_positions:
         graph[x][y] = 1
         if sum(graph[x]) == 4:
             graph.pop(x)
             point += 1
 
-    if not graph: graph.append([0, 0, 0, 0])
-
     while len(graph) > 4:
         graph.pop(0)
 
 n = int(input())
-steps = [list(map(int, input().split())) for _ in range(n)] # t, x, y
-green = [[0, 0, 0, 0]]
-blue = [[0, 0, 0, 0]]
+steps = [list(map(int, input().split())) for _ in range(n)]
+green, blue = [], []
 point = 0
 
 for step in steps:
