ae35efccd0a0f58cb1283ff66d1bdfc7c5bef89b
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-14 16:13:15 +0900

    https://www.acmicpc.net/problem/14502

--- Implementation/연구소.py
+++ Implementation/연구소.py
@@ -1,53 +1,51 @@
 import sys
 input = sys.stdin.readline
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-max_val = -int(1e9)
-n, m = map(int, input().split())
-graph = []
-new_graph = [[0] * m for _ in range(n)]
-virus = []
-for i in range(n):
-    row = list(map(int, input().split()))
-    graph.append(row)
-    for j in range(len(row)):
-        if row[j] == 2:
-            virus.append((i, j))
+def spreadVirus(x, y):
+    for dx, dy in [(0, 1),(0, -1), (1, 0), (-1, 0)]:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < n and 0 <= ny < m and not new_graph[nx][ny]:
+            new_graph[nx][ny] = 2
+            spreadVirus(nx, ny)
 
-def get_safe():
+def getSafeArea():
     safe_area = 0
     for i in range(n):
         safe_area += new_graph[i].count(0)
     return safe_area
 
-def infect(x, y):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and new_graph[nx][ny] == 0:
-            new_graph[nx][ny] = 2
-            infect(nx, ny)
-
-def dfs(start, count):
+def putWall(start, cnt):
     global max_val
-    if count == 3:
+
+    if cnt == 3:
         for i in range(n):
             for j in range(m):
                 new_graph[i][j] = graph[i][j]
-        for i, j in virus:
-            infect(i, j)
-        max_val = max(max_val, get_safe())
+        for x, y in virus:
+            spreadVirus(x, y)
+        max_val = max(max_val, getSafeArea())
         return
-    for i in range(start, n*m):
+
+    for i in range(start, n * m):
         x = i // m
         y = i % m
-        if graph[x][y] == 0:
+        if not graph[x][y]:
             graph[x][y] = 1
-            count += 1
-            dfs(i+1, count)
+            putWall(i + 1, cnt + 1)
             graph[x][y] = 0
-            count -= 1
 
-dfs(0, 0)
+n, m = map(int, input().split())
+graph = []
+new_graph = [[0] * m for _ in range(n)]
+virus = []
+max_val = 0
+
+for i in range(n):
+    row = list(map(int, input().split()))
+    graph.append(row)
+    for j in range(len(row)):
+        if row[j] == 2: virus.append((i, j))
+
+putWall(0, 0)
 print(max_val)
