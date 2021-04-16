b32874530c402de09625059533d130940d228f0e
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-16 19:02:19 +0900

    https://www.acmicpc.net/problem/20058

--- Implementation/마법사_상어와_파이어스톰.py
+++ Implementation/마법사_상어와_파이어스톰.py
@@ -1,57 +1,65 @@
 import sys
-sys.setrecursionlimit(10**5)
+from collections import deque
 input = sys.stdin.readline
 
-def storm(s_len):
-    if s_len == 1: turned = graph.copy()
-    else:
-        turned = [[0] * _len for _ in range(_len)]
-        for i in range(0, _len, s_len):
-            for j in range(0, _len, s_len):
-                for r in range(s_len):
-                    for c in range(s_len):
-                        nr = i + c
-                        nc = j + s_len - r - 1
-                        turned[nr][nc] = graph[i + r][j + c]
+def turn(sep):
+    new_graph = [[0] * n for _ in range(n)]
+    for i in range(0, n, sep):
+        for j in range(0, n, sep):
+            for r in range(sep):
+                for c in range(sep):
+                    new_graph[i + c][j + sep - r - 1] = graph[i + r][j + c]
+    return new_graph
 
-    new_graph = [[0] * _len for _ in range(_len)]
-    for i in range(_len):
-        for j in range(_len):
-            if not turned[i][j]: continue
-            cnt = 0
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+def melt():
+    new_graph = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if not graph[i][j]: continue
+
+            cnt = 4
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 ni = i + dx
                 nj = j + dy
-                if 0 <= ni < _len and 0 <= nj < _len and turned[ni][nj]: cnt += 1
-            if cnt < 3: new_graph[i][j] = turned[i][j] - 1
-            else: new_graph[i][j] = turned[i][j]
+                if not 0 <= ni < n or not 0 <= nj < n or not graph[ni][nj]: cnt -= 1
+                if cnt < 3: break
+
+            if cnt >= 3: new_graph[i][j] = graph[i][j]
+            else: new_graph[i][j] = graph[i][j] - 1
 
     return new_graph
 
-def dfs(x, y):
-    size = 1
+def bfs(x, y):
+    cnt = 1
+    q.append((x, y))
     graph[x][y] = 0
-    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        nx = x + dx
-        ny = y + dy
-        if 0 <= nx < _len and 0 <= ny < _len and graph[nx][ny]:
-            size += dfs(nx, ny)
+    while q:
+        r, c = q.popleft()
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nr = r + dx
+            nc = c + dy
+            if 0 <= nr < n and 0 <= nc < n and graph[nr][nc]:
+                graph[nr][nc] = 0
+                q.append((nr, nc))
+                cnt += 1
 
-    return size
+    return cnt
 
 n, q = map(int, input().split())
-_len = pow(2, n)
-graph = [list(map(int, input().split())) for _ in range(_len)]
-big_chunk = 0
+n = 2 ** n
+graph = [list(map(int, input().split())) for _ in range(n)]
+q = deque()
+ans = 0
 
 for step in list(map(int, input().split())):
-    graph = storm(pow(2, step))
+    if step: graph = turn(2 ** step)
+    graph = melt()
 
 print(sum(map(sum, graph)))
 
-for i in range(_len):
-    for j in range(_len):
+for i in range(n):
+    for j in range(n):
         if graph[i][j]:
-            big_chunk = max(big_chunk, dfs(i, j))
+            ans = max(ans, bfs(i, j))
 
-print(big_chunk)
+print(ans)
