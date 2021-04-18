18f1b9843635b174df94353dc0f19657387c086d
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-18 22:36:29 +0900

    https://www.acmicpc.net/problem/16234

--- Implementation/인구_이동.py
+++ Implementation/인구_이동.py
@@ -2,47 +2,53 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n, l, r = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-q = deque()
-next_q = deque()
-tot_move = 0
-
 def bfs(i, j):
-    _sum = graph[i][j]
+    visited[i][j] = 1
     union = [(i, j)]
+    _sum = graph[i][j]
     q.append((i, j))
     while q:
         x, y = q.popleft()
-        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nx = x + dx
             ny = y + dy
             if 0 <= nx < n and 0 <= ny < n and l <= abs(graph[x][y] - graph[nx][ny]) <= r and not visited[nx][ny]:
+                visited[nx][ny] = 1
                 q.append((nx, ny))
-                union.append([nx, ny])
+                union.append((nx, ny))
                 _sum += graph[nx][ny]
-                visited[nx][ny] = True
-    if len(union) >= 2:
-        ppl = _sum // len(union)
+
+    if len(union) > 1:
+        avg = _sum // len(union)
         for x, y in union:
-            graph[x][y] = ppl
-            next_q.append((x, y))
+            graph[x][y] = avg
+            _next.append((x, y))
         return True
+
     return False
+
+n, l, r = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = []
+_next = deque()
+q = deque()
+cnt = 0
 
 for i in range(n):
     for j in range(n):
-        next_q.append((i, j))
+        _next.append((i, j))
 
 while True:
-    visited = [[False] * n for _ in range(n)]
     is_moved = False
-    for _ in range(len(next_q)):
-        i, j = next_q.popleft()
-        if not visited[i][j]:
-            visited[i][j] = True
-            if bfs(i, j): is_moved = True
-    if not is_moved: break
-    tot_move += 1
+    visited = [[0] * n for _ in range(n)]
 
-print(tot_move)
+    for _ in range(len(_next)):
+        i, j = _next.popleft()
+        if not visited[i][j]:
+            if bfs(i, j): is_moved = True
+
+    if not is_moved:
+        print(cnt)
+        break
+
+    cnt += 1
