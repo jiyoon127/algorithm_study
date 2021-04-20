9a1d28c92e825c16097d848716ffd7ef2681c64f
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-20 19:58:34 +0900

    https://www.acmicpc.net/problem/17143

--- Implementation/낚시왕.py
+++ Implementation/낚시왕.py
@@ -1,51 +1,66 @@
 import sys
 input = sys.stdin.readline
 
-ans = 0
 r, c, m = map(int, input().split())
-graph = [[0] * c for _ in range(r)]
-for _ in range(m):
-  i, j, s, d, z = map(int, input().split())
-  graph[i-1][j-1] = [s, d, z]
-
+sharks = {}
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
+caught = 0
 
-def catch(r, c):
-  for j in range(r):
-    if graph[j][c]:
-      weight = graph[j][c][2]
-      graph[j][i] = 0
-      return weight
-  return 0
+for _ in range(m):
+    sr, sc, s, d, z = map(int, input().split())
+    sr -= 1
+    sc -= 1
+    d -= 1
+    if d in (0, 1): s = s % ((r - 1) * 2)
+    else: s = s % ((c - 1) * 2)
+    if sc in sharks: sharks[sc][sr] = (s, d, z)
+    else: sharks[sc] = {sr: (s, d, z)}
 
-def turn(direct):
-  if direct == 1: direct = 2
-  elif direct == 2: direct = 1
-  elif direct == 3: direct = 4
-  elif direct == 4: direct = 3
-  return direct
-  
-def move(i, j, shark, new_graph):
-  s, d, z = shark
-  for _ in range(s):
-    nx = i + dx[d-1]
-    ny = j + dy[d-1]
-    if 0 <= nx < r and 0 <= ny < c: 
-      i, j = nx, ny
-      continue
-    d = turn(d)
-    i, j = i + dx[d-1], j + dy[d-1]
+for fisher in range(c):
+    new_sharks = {}
+    target = ()
+    if fisher in sharks:
+        row = sorted(sharks[fisher].keys())[0]
+        caught += sharks[fisher][row][2]
+        target = (fisher, row)
 
-  if not new_graph[i][j] or (new_graph[i][j] and new_graph[i][j][2] < z):
-    new_graph[i][j] = [s, d, z]
+    if fisher == c - 1: break
+    for sc in sharks:
+        for sr in sharks[sc]:
+            if (sc, sr) == target: continue
+            s, d, z = sharks[sc][sr]
+            if d in (0, 1):
+                nr = sr + dx[d] * s
+                for _ in range(2):
+                    if nr < 0:
+                        nr = -nr
+                        if d == 0: d = 1
+                        else: d = 0
+                    elif nr >= r:
+                        nr = (r - 1) * 2 - nr
+                        if d == 0: d = 1
+                        else: d = 0
+                nc = sc
+            else:
+                nc = sc + dy[d] * s
+                for _ in range(2):
+                    if nc < 0:
+                        nc = -nc
+                        if d == 2: d = 3
+                        else: d = 2
+                    elif nc >= c:
+                        nc = (c - 1) * 2 - nc
+                        if d == 3: d = 2
+                        else: d = 3
+                nr = sr
 
-for i in range(c):
-  ans += catch(r, i)
-  new_graph = [[0] * c for _ in range(r)]
-  for x in range(r):
-    for y in range(c):
-      if graph[x][y]:
-        move(x, y, graph[x][y], new_graph)
-  graph = new_graph
-print(ans)
+            if nc in new_sharks:
+                if nr in new_sharks[nc]:
+                    if new_sharks[nc][nr][2] < z: new_sharks[nc][nr] = (s, d, z)
+                else: new_sharks[nc][nr] = (s, d, z)
+            else: new_sharks[nc] = {nr: (s, d, z)}
+
+    sharks = new_sharks
+
+print(caught)
