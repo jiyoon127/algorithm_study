2ce191729b94fbf29570d9ee4f60c631072fef30
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-04-29 22:27:14 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42748

---
+++ Implementation/k번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    
+    for i, command in enumerate(commands):
+        start, end, target = command
+        tmp = array[start - 1: end]
+        answer.append(sorted(tmp)[target - 1])
+        
+    return answer
