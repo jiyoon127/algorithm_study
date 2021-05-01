b167e57e6f44ae0b712c35d7d994e2f58ec1d8fd
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-05-02 00:11:15 +0900

    https://www.acmicpc.net/problem/20058

--- Implementation/마법사_상어와_파이어스톰.py
+++ Implementation/마법사_상어와_파이어스톰.py
@@ -2,16 +2,19 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def turn(sep):
-    new_graph = [[0] * n for _ in range(n)]
-    for i in range(0, n, sep):
-        for j in range(0, n, sep):
-            for r in range(sep):
-                for c in range(sep):
-                    new_graph[i + c][j + sep - r - 1] = graph[i + r][j + c]
-    return new_graph
+def fireStorm(s_len):
+    global graph
 
-def melt():
+    new_graph = [[0] * n for _ in range(n)]
+
+    for i in range(0, n, s_len):
+        for j in range(0, n, s_len):
+            for x in range(s_len):
+                for y in range(s_len):
+                    new_graph[i + y][j + s_len - x - 1] = graph[i + x][j + y]
+
+    graph = new_graph
+
     new_graph = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
@@ -29,37 +32,38 @@ def melt():
 
     return new_graph
 
-def bfs(x, y):
-    cnt = 1
-    q.append((x, y))
-    graph[x][y] = 0
-    while q:
-        r, c = q.popleft()
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            nr = r + dx
-            nc = c + dy
-            if 0 <= nr < n and 0 <= nc < n and graph[nr][nc]:
-                graph[nr][nc] = 0
-                q.append((nr, nc))
-                cnt += 1
+def findChunk(i, j):
+    q = deque()
+    q.append((i, j))
+    graph[i][j] = 0
+    cur_chunk = 1
 
-    return cnt
+    while q:
+        x, y = q.popleft()
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny]:
+                cur_chunk += 1
+                graph[nx][ny] = 0
+                q.append((nx, ny))
+
+    return cur_chunk
 
 n, q = map(int, input().split())
 n = 2 ** n
 graph = [list(map(int, input().split())) for _ in range(n)]
-q = deque()
-ans = 0
+orders = list(map(int, input().split()))
+chunk = 0
 
-for step in list(map(int, input().split())):
-    if step: graph = turn(2 ** step)
-    graph = melt()
+for order in orders:
+    graph = fireStorm(2 ** order)
 
 print(sum(map(sum, graph)))
 
 for i in range(n):
     for j in range(n):
         if graph[i][j]:
-            ans = max(ans, bfs(i, j))
+            chunk = max(chunk, findChunk(i, j))
 
-print(ans)
+print(chunk)
