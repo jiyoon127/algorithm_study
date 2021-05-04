067c15764bc9cb237b39b18bf191ee61b6b46522
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-05-04 21:58:24 +0900

    https://www.acmicpc.net/problem/16236

--- DFS|BFS/아기상어.py
+++ DFS|BFS/아기상어.py
@@ -2,47 +2,46 @@ import sys
 import heapq
 input = sys.stdin.readline
 
+def move():
+    seconds = 0
+    ate = 0
+    visited = [[0] * n for _ in range(n)]
+    q = []
+    heapq.heappush(q, (0, shark[1], shark[2]))
+
+    while q:
+        d, x, y = heapq.heappop(q)
+        if 0 < graph[x][y] < shark[0]:
+            ate += 1
+            graph[x][y] = 0
+            if ate == shark[0]:
+                shark[0] += 1
+                ate = 0
+            seconds += d
+            d = 0
+            q = []
+            visited = [[0] * n for _ in range(n)]
+
+        for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            nd = d + 1
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] <= shark[0] and not visited[nx][ny]:
+                visited[nx][ny] = 1
+                heapq.heappush(q, (nd, nx, ny))
+
+    return seconds
+
 n = int(input())
-shark = []
 graph = []
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+shark = tuple()
 
 for i in range(n):
     row = list(map(int, input().split()))
     graph.append(row)
-    for j in range(n):
+    for j in range(len(row)):
         if row[j] == 9:
-            shark = [i, j, 2]
+            shark = [2, i, j]
             graph[i][j] = 0
 
-def moveShark():
-    ate = 0
-    seconds = 0
-    visited = [[False] * n for _ in range(n)]
-    q = []
-    heapq.heappush(q, (0, shark[0], shark[1]))
-
-    while q:
-        d, x, y = heapq.heappop(q)
-        if 0 < graph[x][y] < shark[2]:
-            ate += 1
-            graph[x][y] = 0
-            if ate == shark[2]:
-                ate = 0
-                shark[2] += 1
-            seconds += d
-            d = 0
-            q = []
-            visited = [[False] * n for _ in range(n)]
-        for i in range(4):
-            nd = d + 1
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if not 0 <= nx < n or not 0 <= ny < n: continue
-            if graph[nx][ny] > shark[2] or visited[nx][ny]: continue
-            visited[nx][ny] = True
-            heapq.heappush(q, (nd, nx, ny))
-    print(seconds)
-
-moveShark()
+print(move())
