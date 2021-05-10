23cea78e9c987e46287ab91709f21a65c4acc002
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2021-05-10 21:22:04 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42579

--- Hash/베스트앨범.js
+++ Hash/베스트앨범.js
@@ -3,23 +3,22 @@ function solution(genres, plays) {
     const map = new Map();
     const list = new Map();
     
-    genres.forEach((_,i)=>{
-        if(!map.get(genres[i])){
-               map.set(genres[i],plays[i]);
-               list.set(genres[i],[{num:plays[i],index:i}]);
-           }
-        else{
-            map.set(genres[i],map.get(genres[i])+plays[i]);
+    genres.forEach((_, i) => {
+        if (!map.get(genres[i])) {
+            map.set(genres[i], plays[i]);
+            list.set(genres[i], [{num:plays[i], index:i}]);
+        } else {
+            map.set(genres[i], map.get(genres[i]) + plays[i]);
             list.set(genres[i], list.get(genres[i])
-                     .concat([{num:plays[i],index:i}]));
+                     .concat([{num:plays[i], index:i}]));
         }
     });
     
     let arr = [...map.entries()];
-    arr = arr.sort((a,b)=>{return b[1]-a[1]})
-    arr.forEach((item)=>{
-        let top2 = list.get(item[0]).sort((a,b)=>{return b.num-a.num}).slice(0,2);
-        top2.forEach((t)=>{
+    arr = arr.sort((a,b) => b[1] - a[1])
+    arr.forEach((item) => {
+        let top2 = list.get(item[0]).sort((a,b) => b.num - a.num).slice(0, 2);
+        top2.forEach((t) => {
             answer = answer.concat(t.index);
         });
     });
